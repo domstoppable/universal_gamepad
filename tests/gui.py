7ba8ad0d0cb3b90684074870822407c690c2599d
Dominic Canare <dom@greenlightgo.org>
2022-06-30 14:57:16 -0500

    Add/remove controller widgets dynamically

--- tests/gui.py
+++ tests/gui.py
@@ -4,21 +4,44 @@ from gamepad.eventDaemon import *
 from gamepad.gamepad import *
 from gamepad.ui import ControllerWidget
 
+padWidgets = {}
+
+def removeWidget(widget):
+		window.layout().takeAt(window.layout().indexOf(widget))
+		widget.setParent(None)
+
 def onPadConnected(pad):
-	frame = ControllerWidget()
-	frame.bindToGamepad(pad)
-	window.layout().addWidget(frame)
+	padWidgets[pad] = ControllerWidget()
+	padWidgets[pad].bindToGamepad(pad)
+	window.layout().addWidget(padWidgets[pad])
+
+	window.layout().removeWidget(pluginLabel)
+	pluginLabel.setParent(None)
+
+def onPadDisconnected(pad):
+	if pad in padWidgets:
+		window.layout().takeAt(window.layout().indexOf(padWidgets[pad]))
+		padWidgets[pad].setParent(None)
+
+		if window.layout().count() == 0:
+			window.layout().addWidget(pluginLabel)
 
 app = QApplication()
 
 window = QWidget()
 window.setLayout(QHBoxLayout())
-window.show()
+
+pluginLabel = QLabel('Please connect a controller')
+pluginLabel.setAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
+pluginLabel.setMargin(200)
+window.layout().addWidget(pluginLabel)
 
 daemon = GamepadDaemon()
 daemon.gamepadConnected.connect(onPadConnected)
+daemon.gamepadDisconnected.connect(onPadDisconnected)
 daemon.start()
 
 app.aboutToQuit.connect(daemon.stop)
-app.exec_()
 
+window.show()
+app.exec_()
