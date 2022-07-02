bac359888c1a90e15cb685790dfe5e0f4f28ed8f
Dominic Canare <dom@dominiccanare.com>
2022-07-02 02:10:09 -0500

    Use packaged dll if available

--- universal_gamepad/__init__.py
+++ universal_gamepad/__init__.py
@@ -1,5 +1,9 @@
+import os
 import pkg_resources
 
 def locateAsset(*path):
 	resource = '/'.join(['assets'] + list(path))
 	return pkg_resources.resource_filename(__name__, resource)
+
+os.environ["PYSDL2_DLL_PATH"] = locateAsset('.')
+import sdl2dll
