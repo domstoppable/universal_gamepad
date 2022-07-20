20c7305e30dd3d3e43a69cac40f6d9bc1a72bccd
Dominic Canare <dom@greenlightgo.org>
2022-07-20 13:25:51 -0500

    Include assets for bdist

--- setup.py
+++ setup.py
@@ -10,5 +10,9 @@ setup(
 	author_email='dom@dominiccanare.com',
 	url='https://github.com/domstoppable/universal_gamepad',
 	packages=['universal_gamepad',],
+	package_data={
+		'universal_gamepad': ['universal_gamepad/assets/*'],
+	},
+	include_package_data=True,
 	install_requires=[ 'PySide2', 'pysdl2', 'pysdl2-dll', ],
 )
